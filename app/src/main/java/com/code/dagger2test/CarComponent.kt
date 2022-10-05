55d0efce3c42f88b7fa136e9e1c48c72519c00de
Jamie <cruiseomondi90@gmail.com>
2022-10-06 01:16:26 +0300

    Modules and Providers

--- app/src/main/java/com/code/dagger2test/CarComponent.kt
+++ app/src/main/java/com/code/dagger2test/CarComponent.kt
@@ -2,7 +2,7 @@ package com.code.dagger2test
 
 import dagger.Component
 
-@Component
+@Component(modules = [WheelsModule::class])
 interface CarComponent {
     fun getCar():Car
     fun inject(mainActivity: MainActivity)
