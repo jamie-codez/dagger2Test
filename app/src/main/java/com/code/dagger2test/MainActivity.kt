5959838d1c78677b6ec880320084d1b9c6dc26d1
Jamie <cruiseomondi90@gmail.com>
2022-10-06 00:40:49 +0300

    Private field not injected

--- app/src/main/java/com/code/dagger2test/MainActivity.kt
+++ app/src/main/java/com/code/dagger2test/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
-    @Inject private lateinit var car:Car
+    @Inject lateinit var car:Car
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
